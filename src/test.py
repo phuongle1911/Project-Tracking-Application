8508130765fe5bc0bb48b45d0695f11fd00221cb
Phuong Le <phuongbk3@gmail.com>
2025-06-26 22:50:16 +1000

    Add json test file

--- src/test.py
+++ src/test.py
@@ -1,5 +1,12 @@
-import user_input
+from user_input import SubmitError, UserInput
 import Project_setup
 import cost_input
 import project_summary
 
+class TestUserInput:
+
+  def test_normal_input(self):
+    user_input = UserInput("Testing prompt")
+    assert user_input.get_input() == "Testing prompt"
+  
+  
